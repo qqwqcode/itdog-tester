ab3b7f6d01a241fa9804192270c9c64ea01381bb
qcode <subedenaittt@outlook.sg>
2025-09-04 22:52:33 +0800

    Update runtest.py

--- runtest.py
+++ runtest.py
@@ -14,8 +14,8 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 # 配置参数
 REFRESH_INTERVAL = 2  # 刷新间隔（秒）
-TOTAL_REFRESH_COUNT = 160  # 总刷新次数
-WAIT_AFTER_TEST = 18  # 每次测速后等待时间（秒）
+TOTAL_REFRESH_COUNT = 300  # 总刷新次数
+WAIT_AFTER_TEST = 8  # 每次测速后等待时间（秒）
 
 # 基础User-Agent模板列表
 USER_AGENT_TEMPLATES = [
